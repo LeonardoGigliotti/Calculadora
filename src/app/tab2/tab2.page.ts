07ad0c6491d862b8892cf7b2af214150ee015072
Leonardo Silva Gigliotti <leonardosilvagigliottii@gmail.com>
2022-09-30 20:27:31 -0300

    tab2
    tab2

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -6,7 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
-
-  constructor() {}
-
-}
+  valor1;
+  valor2;
+  cont;
+  result;
+   constructor() {}
+ 
+   add(){
+     this.result = parseInt(this.valor1) + parseInt(this.valor2);
+   }
+   sub(){
+     this.result = parseInt(this.valor1) - parseInt(this.valor2);
+   }
+   mult(){
+     this.result = parseInt(this.valor1) * parseInt(this.valor2);
+   }
+   div(){
+     this.result = parseInt(this.valor1) / parseInt(this.valor2);
+   }
+   conta(){
+     this.cont = this.result;
+   }
+ }
