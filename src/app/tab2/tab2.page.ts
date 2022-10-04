e8755f7ab5bc7e7b35f1ad65490fc71aa825b969
Leonardo Silva Gigliotti <leonardosilvagigliottii@gmail.com>
2022-10-03 21:21:31 -0300

    calc2
    calc2

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,30 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
-  valor1;
-  valor2;
-  cont;
-  result;
-   constructor() {}
- 
-   add(){
-     this.result = parseInt(this.valor1) + parseInt(this.valor2);
-   }
-   sub(){
-     this.result = parseInt(this.valor1) - parseInt(this.valor2);
-   }
-   mult(){
-     this.result = parseInt(this.valor1) * parseInt(this.valor2);
-   }
-   div(){
-     this.result = parseInt(this.valor1) / parseInt(this.valor2);
-   }
-   conta(){
-     this.cont = this.result;
-   }
- }
+export class Tab2Page implements OnInit {
+  operacao = '';
+  resultado = '';
+  
+  constructor() {}
+  
+  ngOnInit() {}
+
+  adicionarValor(valor: any){
+    this.operacao += valor;
+  }
+
+  limparMemoria(){
+    this.operacao = '';
+    this.resultado = '';
+  }
+
+  limparOperacao(){
+    this.operacao = '';
+  }
+
+  inverterValor(){
+    //alterna o numero entre positivo e negativo
+  }
+
+  apagarCaracter(){
+    if (this.operacao.length > 0){
+      this.operacao = this.operacao.substring(0, this.operacao.length - 1);
+    }
+  }
+}
