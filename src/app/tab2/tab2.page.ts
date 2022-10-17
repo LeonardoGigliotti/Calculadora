279bb19f007966a6e649b2ba721291ec7170d483
Leonardo Silva Gigliotti <leonardosilvagigliottii@gmail.com>
2022-10-17 20:37:18 -0300

    .
    .

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { evaluate } from 'mathjs';
 
 @Component({
   selector: 'app-tab2',
@@ -8,22 +9,43 @@ import { Component, OnInit } from '@angular/core';
 export class Tab2Page implements OnInit {
   operacao = '';
   resultado = '';
+  numero = false; 
+  caracter = true;
+  caracteres = ['.', '/', '*', '+', '-'];
   
   constructor() {}
   
   ngOnInit() {}
 
-  adicionarValor(valor: any){
-    this.operacao += valor;
+  realizarOperacao(){
+    try {
+    this.resultado = evaluate(this.operacao);
+    } catch (err) {
+      this.resultado = 'inválido!';
+    }
+  }
+
+  adicionarValor(valor: string){
+    this.caracter = this.caracteres.includes(valor);
+
+    if (!this.caracter) {
+      this.operacao += valor;
+      this.numero = true;
+    } else if (this.caracter && this.numero) {
+      this.operacao += valor;
+      this.numero = false;
+    }
   }
 
   limparMemoria(){
     this.operacao = '';
     this.resultado = '';
+    this.numero = false;
   }
 
   limparOperacao(){
     this.operacao = '';
+    this.numero = false;
   }
 
   inverterValor(){
@@ -33,6 +55,17 @@ export class Tab2Page implements OnInit {
   apagarCaracter(){
     if (this.operacao.length > 0){
       this.operacao = this.operacao.substring(0, this.operacao.length - 1);
+    }
+
+    const ultimo = this.operacao.substring(this.operacao.length, 1);
+    this.caracter = this.caracteres.includes(ultimo);
+    
+    console.log(ultimo);
+
+    if (!this.caracter) {
+      this.numero = true;
+    } else {
+      this.numero = false;
     }
   }
 }
